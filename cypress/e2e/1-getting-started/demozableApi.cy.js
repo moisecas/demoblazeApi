078d28913f99210f2e4e94986a5094ed775f27f3
unknown <moisesjuniorcas20@gmail.com>
2024-01-22 09:00:18 -0500

    creando test

--- cypress/e2e/1-getting-started/demozableApi.cy.js
+++ cypress/e2e/1-getting-started/demozableApi.cy.js
@@ -26,37 +26,14 @@ describe('Demozable.com', function() {
         
     })
 
-    it('carrito de compras', function() {
+    it('usuario y password correcto en login', function() {
         cy.visit('https://www.demoblaze.com/index.html')
-        cy.contains('Samsung galaxy s6').click();
-        cy.get('.col-sm-12 > .btn').click() //agrega al carrito 
-        cy.get('.active > .nav-link').click() //home 
-        cy.contains('Nokia lumia 1520').click()
-        cy.get('.col-sm-12 > .btn').click() //agrega al carrito 
-        cy.get('.active > .nav-link').click() //home 
-        cy.contains('Nexus 6').click()
-        cy.get('.col-sm-12 > .btn').click() //agrega al carrito 
-        cy.get('#cartur').click() //ver carrito 
-        cy.get('#tbodyid > :nth-child(1) > :nth-child(4) > a').click() //delete item
+        
             
     })
-    it('checkout', function() {
+    it('usuario y password incorrecto en login', function() {
         cy.visit('https://www.demoblaze.com/index.html')
-        cy.contains('Samsung galaxy s6').click();
-        cy.get('.col-sm-12 > .btn').click() //agrega al carrito 
-        cy.get('#cartur').click() //ver carrito 
-        cy.get('.col-lg-1 > .btn').click() //place order
-        cy.get('#name').type('Moises');
-        cy.get('#country').type('Colombia')
-        cy.get('#city').type('Bucaramanga')
-        cy.get('#card').type(123456)
-        cy.get('#month').type('octubre')
-        cy.get('#year').type(2023)
-        cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click() //botón de compra
-        //verificación de TC
-        cy.get('#card').invoke('val').then((cardValue) => {
-            cy.get('.lead').should('contain.text', cardValue);
-        });
+       
         
 
         
