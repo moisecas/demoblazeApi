1b87372d6ac0434bc09c0a71336ccaec8e5b4236
unknown <moisesjuniorcas20@gmail.com>
2024-01-22 09:21:46 -0500

    creando test para login

--- cypress/e2e/1-getting-started/demozableApi.cy.js
+++ cypress/e2e/1-getting-started/demozableApi.cy.js
@@ -14,29 +14,48 @@ describe('Demozable.com', function() {
     }
     )
 
-    it.only('Intentar crear un usuario existente moisecas32', function() {
+    it('Intentar crear un usuario existente moisecas32', function() {
       // Visita la página 
     cy.visit('https://www.demoblaze.com/index.html')
-    cy.get('#signin2').click()
-    cy.get('#sign-username').type('moisecas32')
-    cy.get('#sign-password').type('tuContraseña') 
-    cy.on('window:alert', (alertText) => {  //validar ventana alert 
-        expect(alertText).to.contains('This user already exist');
-    });              
+    cy.get('#signin2').should('be.visible').click()
+
+    // Esperar, encontrar y escribir en el campo de nombre de usuario
+    cy.get('#sign-username').should('be.visible').type('moisecas32')
+
+    // Esperar, encontrar y escribir en el campo de contraseña
+    cy.get('#sign-password').should('be.visible').type('tuContraseña')
+
+    // Hacer clic en el botón para crear la cuenta (ajusta el selector según sea necesario)
+    cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()    
+    
+    cy.on('window:alert', (alertText) => {
+        expect(alertText).to.contains('Please fill out Username and Password.');
+    });
         
     })
 
-    it('usuario y password correcto en login', function() {
+    it.only('usuario y password correcto en login', function() {
         cy.visit('https://www.demoblaze.com/index.html')
+        cy.get('#login2').click()
+        cy.get('#loginusername').type('moisecas32') 
+        cy.get('#loginpassword').type(123456) 
+        cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
         
             
     })
-    it('usuario y password incorrecto en login', function() {
+    it('usuario incorrecto en login', function() {
         cy.visit('https://www.demoblaze.com/index.html')
-       
-        
+        cy.get('#login2').click()
+        cy.get('#loginusername').type('moisecas3')    //el usuario correcto es moisecas32                  
+            
+    })
 
-        
+    it('password incorrecto en login', function() {
+        cy.visit('https://www.demoblaze.com/index.html')
+        cy.get('#login2').click()
+        cy.get('#loginusername').type('moisecas32') 
+        cy.get('#loginpassword').type(12345) //el password correcto es 123456 
+        cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()                     
             
     })
     
